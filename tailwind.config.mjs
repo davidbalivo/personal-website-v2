52378bd99aa28f84c431d01ea2b00547c43a7848
David Balivo <davidbalivo@Davids-MacBook-Pro.local>
2024-11-26 22:30:08 +0100

    feat: improve responsiveness with new 'ms' breakpoint between md and sm

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,18 +3,10 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   safelist: [
     {
-      pattern: /^(md:)?(p|px|py|pt|pb|pl|pr)-\d+$/,
+      pattern: /^(p|px|py|pt|pb|pl|pr)-\d+$/,
     },
   ],
   theme: {
-    screens: {
-      custom: '995px',
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      '2xl': '1536px',
-    },
     extend: {
       colors: {
         primary: '#F9FAFB',
@@ -24,6 +16,10 @@ export default {
           light: '#66C5E3',
         },
         background: '#0A0A0A',
+      },
+      screens: {
+        ms: '700px',
+        custom: '995px',
       },
     },
   },
