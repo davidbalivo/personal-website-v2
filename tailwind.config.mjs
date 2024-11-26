46b9d39b9661ff699a6f9cfe9ecac0944587887d
David Balivo <davidbalivo@Davids-MacBook-Pro.local>
2024-11-26 17:27:36 +0100

    feat: improve responsiveness across layouts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  safelist: [
+    {
+      pattern: /^(md:)?(p|px|py|pt|pb|pl|pr)-\d+$/,
+    },
+  ],
   theme: {
     extend: {
       colors: {
