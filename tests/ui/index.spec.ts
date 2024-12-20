fd98574b7f13aabcd43d0dd2b5c1e9e238b4067e
David Balivo <davidbalivo@Davids-MacBook-Pro.local>
2024-12-20 10:00:45 +0100

    add 1% maxDiffPixelRatio to visual regression testing for cross-system flexibility

--- tests/ui/index.spec.ts
+++ tests/ui/index.spec.ts
@@ -17,6 +17,7 @@ test.describe(
       await expect(page).toHaveScreenshot(`page-home-${test.info().project.name}.png`, {
         fullPage: true,
         threshold: 0.01,
+        maxDiffPixelRatio: 0.01,
       });
     });
 
@@ -25,6 +26,7 @@ test.describe(
       await expect(page).toHaveScreenshot(`page-experience-${test.info().project.name}.png`, {
         fullPage: true,
         threshold: 0.01,
+        maxDiffPixelRatio: 0.01,
       });
     });
   },
